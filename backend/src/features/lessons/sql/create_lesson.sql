31c22287e6920b7c3e060bb60057f9bb8f78f03e
Nabe_kz <ayuzak4me38@gmail.com>
2026-05-01 09:07:45 +0900

    feat: add squirrel

---
+++ backend/src/features/lessons/sql/create_lesson.sql
@@ -0,0 +1,6 @@
+INSERT INTO lessons
+  (id, name, instructor, starts_at, ends_at, capacity, remaining_slots, description)
+VALUES
+  ($1, $2, $3, $4, $5, $6, $7, $8)
+RETURNING
+  id, name, instructor, starts_at, ends_at, capacity, remaining_slots, description
